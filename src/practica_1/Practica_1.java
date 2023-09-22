14db269c2d3b2cabb8f9391f1bdf82684e29223f
AdrianLiraMcfly <jorge.liralopez11@gmail.com>
2023-09-22 16:49:23 -0600

    si

---
+++ src/practica_1/Practica_1.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package practica_1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jorge
+ */
+public class Practica_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner= new Scanner(System.in);
+        
+        String cadena;
+        int est=0, i=0;     
+        boolean conti=true;
+        
+        System.out.print("Ingresa la cadena:");
+        cadena=scanner.next();
+        char[] c = new char[cadena.length()];
+        while(conti=true&&c!=null){
+            switch(est){
+                case 0:
+                    if(c==)
+            }
+        }
+    }
+    
+}
